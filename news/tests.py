857991bbc2f87a3337d33dd1b08df8f02e5bfcdd
Yan-ren Tsai <elleryq@gmail.com>
2020-09-20 21:13:26 +0800

    Try pytest / pytest-sugar

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Article
 
-# Create your tests here.
+
+class ArticleTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_article_is_published(self):
+        """Animals that can speak are correctly identified"""
+        self.assertEqual(True, True)
